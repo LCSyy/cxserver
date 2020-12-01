e8cf11619146a5fc50116eee4a005df1ceb363f1
chuns <chuns.liu@foxmail.com>
2020-12-01 22:01:11 +0800

    use sqlx

--- cxserver/src/main.rs
+++ cxserver/src/main.rs
@@ -1,18 +1,31 @@
+use std::env;
+use sqlx::postgres::PgPoolOptions;
 use actix_web::{HttpServer,App,web};
 use actix_rt;
-
-const HTTP_SERVER_ADDR: &'static str = "127.0.0.1:8001";
+use dotenv;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&env::var("DATABASE_URL").unwrap())
+        .await
+        .unwrap();
+
     HttpServer::new(
-        || App::new().route("/api", web::get().to(api))
+        move || App::new().data(pool.clone()).route("/api", web::get().to(api))
     )
-    .bind(HTTP_SERVER_ADDR)?
+    .bind(dotenv::var("SERVER_ADDR").unwrap())?
     .run()
     .await
 }
 
-async fn api() -> String {
-    String::from("Hello CxServer !")
+async fn api(pool: web::Data<sqlx::PgPool>) -> String {
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&*pool.into_inner()).await.unwrap();
+
+    row.0.to_string()
 }
